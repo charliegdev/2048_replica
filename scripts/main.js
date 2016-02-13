f32bd9b1d530e9ea788f96e602dfe52e5a923c26
Charlie Guan <charliegdev@gmail.com>
2016-02-13 13:17:39 -0800

    Add basic arrow keypress event handler.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,33 @@
 /**
  * Created by charlieguan on 2016-02-13.
  */
+
+
+
+
+
+
+$(document).ready(main);
+
+function main() {
+    $(document).keydown(function (event) {
+        switch (event.which) {
+            // up
+            case 38:
+                console.log('up');
+                break;
+            // down
+            case 40:
+                console.log('down');
+                break;
+            // left
+            case 37:
+                console.log('left');
+                break;
+            // right
+            case 39:
+                console.log('right');
+                break;
+        }
+    });
+}
