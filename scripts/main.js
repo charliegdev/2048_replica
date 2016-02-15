b17b97c8cd3e21ea00a90586a6faf538e05750a4
Charlie Guan <charliegdev@gmail.com>
2016-02-14 18:52:52 -0800

    Handle basic tile movement; spawn a tile at the top left corner when start a new game.

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,42 @@
 /**
  * Created by charlieguan on 2016-02-13.
  */
-
-
-
-
-
-
+/*  Some dimensions:
+ Each tile is 104px * 104px;
+ Each movement is 109px long.
+ Spawning: distance to any anchor point: 4px top, 4px left.
+ */
 $(document).ready(main);
 
 function main() {
-    $(document).keydown(function (event) {
-        switch (event.which) {
-            // up
-            case 38:
-                console.log('up');
-                break;
-            // down
-            case 40:
-                console.log('down');
-                break;
-            // left
-            case 37:
-                console.log('left');
-                break;
-            // right
-            case 39:
-                console.log('right');
-                break;
-        }
-    });
+    placeSpawningTile();
+    $(document).keydown(moveAllTiles);
+}
+function moveAllTiles(event) {
+    var $tile = $("tile");
+    switch (event.which) {
+
+        // up
+        case 38:
+            $tile.animate({marginTop: '-=109px'}, 100);
+            break;
+        // down
+        case 40:
+            $tile.animate({marginTop: '+=109px'}, 100);
+            break;
+        // left
+        case 37:
+            $tile.animate({marginLeft: '-=109px'}, 100);
+            break;
+        // right
+        case 39:
+            $tile.animate({marginLeft: '+=109px'}, 100);
+            break;
+    }
+}
+
+function placeSpawningTile() {
+    $('<div/>', {
+        class: 'tile'
+    }).appendTo('#gameBoard');
 }
