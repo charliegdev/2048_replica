2a73d22624bd5e74beaeb06e511e9efaaca9b068
Charlie Guan <charliegdev@gmail.com>
2016-02-15 00:01:38 -0800

    Add functionality to randomly generate 2 tiles in different cells.

--- scripts/main.js
+++ scripts/main.js
@@ -35,7 +35,29 @@ function moveAllTiles(event) {
 }
 
 function placeSpawningTile() {
-    $('<div/>', {
-        class: 'tile'
-    }).appendTo('#gameBoard');
+    var $gameBoard = $('#gameBoard');
+    /*
+     randomly generate 2 sets of coordinates. Note: the following method has a flaw:
+     say if we want to generate a random number between [0, 3]; the probability of getting
+     a 1 is the lowest (about 18%); the probability of getting the other 3 are roughly equal.
+     */
+    var col1 = Math.floor(Math.random() * 4),
+        row1 = Math.floor(Math.random() * 4),
+        col2 = Math.floor(Math.random() * 4),
+        row2 = Math.floor(Math.random() * 4);
+
+    // make sure those 2 tiles are not on top of each other!
+    while (col1 === col2 && row1 === row2) {
+        col2 = Math.floor(Math.random() * 4);
+        row2 = Math.floor(Math.random() * 4);
+    }
+
+    // generate 2 tiles using the coordinates generated.
+    var $tile1 = $("<div>", {class: "tile"});
+    $tile1.addClass("tile" + col1 + row1);
+    $gameBoard.append($tile1);
+
+    var $tile2 = $("<div>", {class: "tile"});
+    $tile2.addClass("tile" + col2 + row2);
+    $gameBoard.append($tile2);
 }
