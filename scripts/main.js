2d83184cf58da16c74bf63fa2d338a31e7a1427f
Charlie Guan <charliegdev@gmail.com>
2016-02-22 02:58:35 -0800

    Add mechanism for collision detection. However in some rare time, this does not work. I suspect it's caused by race conditions. Also, corrected row/column confusion. For a coordinate, the 1st number is the row number, the second number is the column number.

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@
  Each movement is 109px long.
  Spawning: distance to any anchor point: 4px top, 4px left.
  */
+"use strict";
 $(document).ready(main);
 
 function main() {
@@ -15,78 +16,119 @@ function main() {
 }
 
 function moveAllTiles(event) {
-    var MOVE_DISTANCE = 109;
+    const MOVE_DISTANCE = 109;
+    switch (event.which) {
+        /*  TODO: when an arrow key is pressed, instead of using $(".tile").each(moveOneTile), we should do it row by row,
+         or column by column.
+         */
+
+        // up
+        case 38:
+
+            break;
+        // down
+        case 40:
+
+            break;
+        // left
+        case 37:
+
+            break;
+        // right
+        case 39:
+
+            break;
+    }
 
     $(".tile").each(moveOneTile);
 
-
     function moveOneTile(index) {
-        var currentPosition = {
-                column: null,
-                row   : null
+        let currentPosition = {
+                row   : null,
+                column: null
             },
             $this           = $(this),
             oldClassName,
             newClassName;
 
         // get the 1st and 2nd digit of the current coordinate class name
-        currentPosition.column = $this.attr('class').split(' ')[1][0];
-        currentPosition.row    = $this.attr('class').split(' ')[1][1];
+        currentPosition.row    = $this.attr('class').split(' ')[1][0];
+        currentPosition.column = $this.attr('class').split(' ')[1][1];
 
-        oldClassName = currentPosition.column + '' + currentPosition.row;
+        oldClassName = currentPosition.row + '' + currentPosition.column;
         $this.removeClass(oldClassName);
 
         switch (event.which) {
             // up
             case 38:
-                if (currentPosition.column > 0) {
+                if (currentPosition.row > 0) {
                     //console.log(currentPosition.column);
                     //$this.animate({marginTop: '-=109px'}, 100);
                     //currentPosition.column--;
-                    moveToEdge($this, 'up', currentPosition.column);
-                    currentPosition.column = 0;
+                    currentPosition.row = moveToEdge($this, 'up', currentPosition.row, currentPosition.column);
                 }
                 break;
             // down
             case 40:
-                if (currentPosition.column < 3) {
-                    moveToEdge($this, 'down', currentPosition.column);
-                    currentPosition.column = 3;
+                if (currentPosition.row < 3) {
+                    currentPosition.row = moveToEdge($this, 'down', currentPosition.row, currentPosition.column);
                 }
                 break;
             // left
             case 37:
-                if (currentPosition.row > 0) {
-                    moveToEdge($this, 'left', currentPosition.row);
-                    currentPosition.row = 0;
+                if (currentPosition.column > 0) {
+                    currentPosition.column = moveToEdge($this, 'left', currentPosition.row, currentPosition.column);
                 }
                 break;
             // right
             case 39:
-                if (currentPosition.row < 3) {
-                    moveToEdge($this, 'right', currentPosition.row);
-                    currentPosition.row = 3;
+                if (currentPosition.column < 3) {
+                    currentPosition.column = moveToEdge($this, 'right', currentPosition.row, currentPosition.column);
                 }
                 break;
         }
-        newClassName = currentPosition.column + '' + currentPosition.row;
+        console.log(currentPosition.row, currentPosition.column);
+        newClassName = currentPosition.row + '' + currentPosition.column;
         $this.addClass(newClassName);
     }
 
     //function tileNameWildCard(index, className) {
     //    return (className.match(/[0-3][0-3]/g) || []).join(' ');
     //}
-    function moveToEdge(tile, direction, gridNum) {
+    function moveToEdge(tile, direction, rowNum, columnNum) {
         if (direction === 'up') {
-            tile.animate({marginTop: '-=' + gridNum * MOVE_DISTANCE + 'px'}, 100);
+            for (let i = 0; i < rowNum; i++) {
+                if ($(`.${i}${columnNum}`).length === 0) {
+                    tile.animate({marginTop: '-=' + (rowNum - i) * MOVE_DISTANCE + 'px'}, 100);
+                    return i;
+                }
+            }
+
         } else if (direction === 'down') {
-            tile.animate({marginTop: '+=' + (3 - gridNum) * MOVE_DISTANCE + 'px'}, 100);
+            for (let i = 3; i > rowNum; i--) {
+                if ($(`.${i}${columnNum}`).length === 0) {
+                    tile.animate({marginTop: '+=' + (i - rowNum) * MOVE_DISTANCE + 'px'}, 100);
+                    return i;
+                }
+            }
         } else if (direction === 'left') {
-            tile.animate({marginLeft: '-=' + gridNum * MOVE_DISTANCE + 'px'}, 100);
+            for (let i = 0; i < columnNum; i++) {
+                if ($(`.${rowNum}${i}`).length === 0) {
+                    tile.animate({marginLeft: '-=' + (columnNum - i) * MOVE_DISTANCE + 'px'}, 100);
+                    return i;
+                }
+            }
+            //tile.animate({marginLeft: '-=' + rowNum * MOVE_DISTANCE + 'px'}, 100);
         } else if (direction === 'right') {
-            tile.animate({marginLeft: '+=' + (3 - gridNum) * MOVE_DISTANCE + 'px'}, 100);
+            for (let i = 3; i > columnNum; i--) {
+                if ($(`.${rowNum}${i}`).length === 0) {
+                    tile.animate({marginLeft: '+=' + (i - columnNum) * MOVE_DISTANCE + 'px'}, 100);
+                    return i;
+                }
+            }
         }
     }
+
 }
 
 function spawnNewTile() {
@@ -95,12 +137,12 @@ function spawnNewTile() {
      say if we want to generate a random number between [0, 3]; the probability of getting
      a 1 is the lowest (about 18%); the probability of getting the other 3 are roughly equal.
      */
-    var colNum          = Math.floor(Math.random() * 4),
+    let colNum          = Math.floor(Math.random() * 4),
         rowNum          = Math.floor(Math.random() * 4),
         colDisplacement = colNum * 109,
         rowDisplacement = rowNum * 109;
     // create 1 new tile using the coordinates generated.
-    var $newTile = $("<div>", {class: "tile"});
+    let $newTile = $("<div>", {class: "tile"});
     $newTile.addClass(colNum + rowNum); // column based coordinate used as class name.
     $('#gameBoard').append($newTile);
 
@@ -109,6 +151,6 @@ function spawnNewTile() {
         marginTop : '+=' + colDisplacement + 'px',
         marginLeft: '+=' + rowDisplacement + 'px'
     }, 0);
-    var className = colNum + '' + rowNum;
+    let className = colNum + '' + rowNum;
     $newTile.addClass(className);
 }
